79c94c02bababf73a3ee935dc8476c6ee52f219a
Aaron Cleveland <toscleveland@gmail.com>
2022-04-27 12:37:50 -0500

    Added Chapter 4: Stacks

---
+++ SwiftUIStacks/SwiftUIStacks/SwiftUIStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStacksApp.swift
+//  SwiftUIStacks
+//
+//  Created by Aaron Cleveland on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
